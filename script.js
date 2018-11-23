6d145a740bc55391e8391340fb30f78552ac1518
Mihail <mihailfusion@gmail.com>
2018-11-24 01:27:08 +0300

    шаблон

---
+++ script.js
@@ -0,0 +1,29 @@
+function myFirstApp(name, age) {
+  alert("Привет, меня зовут " + name + " и это моя первая программа!")
+
+  function ahowSkills() {
+    let skills = [];
+
+    for (let i = 0; i , skills.length; i++) {
+
+    }
+  }
+
+  showSkills();
+
+  function checkAge() {
+    if (age > 18) {
+
+    } else {
+
+    }
+    
+  }
+
+  function calcPow(num) {
+    
+  }
+  calcPow(4)
+}
+
+myFirstApp("Mikail", 27)
