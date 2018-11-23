3996817c3ca01e01218067b70e0f97c7dfb9343f
Mihail <mihailfusion@gmail.com>
2018-11-24 02:40:14 +0300

    done

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 function myFirstApp(name, age) {
   alert("Привет, меня зовут " + name + " и это моя первая программа!")
 
-  function ahowSkills() {
-    let skills = [];
+  // let age = prompt("Сколько Вам лет");
 
-    for (let i = 0; i , skills.length; i++) {
+  function checkAge() {
+    let age = prompt("Сколько Вам лет");
+    if (age >= 18) {
+      alert("Ты уже взрослый - заходи");
 
+    } else {
+      alert("Маловат еще, посмотри мультики");
+      window.location.href = "http://onlinemultfilmy.ru";
+    }
+    
+  }
+  checkAge();
+
+
+  function showSkills() {
+    let skills = ["Вот что я могу:", "HTML", "CSS", "SASS", "JQuery", "GIT", "И МНОГОЕ ДРУГОЕ))"
+    ];
+
+    for (let i = 0; i < skills.length; i++) {
+      document.write(skills[i], "<br>");
     }
   }
 
   showSkills();
-
-  function checkAge() {
-    if (age > 18) {
-
-    } else {
-
-    }
-    
-  }
+ 
+ 
 
   function calcPow(num) {
-    
+    document.write("квадрат ", num, " = ", num * num)
   }
-  calcPow(4)
+  calcPow(4);
 }
 
-myFirstApp("Mikail", 27)
+myFirstApp("Mikail");
